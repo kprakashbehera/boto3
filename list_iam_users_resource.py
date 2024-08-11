06bc6d325d9a9b2985d51d4cda76d26a101d3ff8
pb01146 <prakashkumar.behera@randstad.com>
2024-08-11 23:14:59 +0200

    list iam users with resource

--- list_iam_users_resource.py
+++ list_iam_users_resource.py
@@ -4,6 +4,7 @@ aws_mgmt_con = boto3.session.Session(profile_name='kpb.devops')
 iam_session = aws_mgmt_con.resource('iam')
 
 # listing users using 'resource'
+print('list of iam users')
 for user in iam_session.users.all():
-    print(user)
+  #  print(user)
     print(user.name)
